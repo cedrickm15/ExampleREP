4b2ad00e23533f9f494e6ea27944426ba18989a1
cedrickm15 <105075042+cedrickm15@users.noreply.github.com>
2023-09-06 12:53:02 -0700

    Update program.py
    Password Deleted!

--- program.py
+++ program.py
@@ -1,5 +1,3 @@
-def test_password(x):
-    print(x)
 
 a = "String 1"
 b = "String 2"
@@ -16,5 +14,3 @@ job_list = {'John': 'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
 for name, job in job_list.items():
     print(f"{name} is a {job}")
 
-Password ='SuperSecretAdminPass'
-test_password(Password)    
